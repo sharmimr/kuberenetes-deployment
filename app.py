35f5643fbfef593de5716793eeccec33a70b3670
sharmimr <62257567+sharmimr@users.noreply.github.com>
2024-08-05 00:56:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/deliveryConfirmation', methods=['GET'])
+def delivery_confirmation():
+    return jsonify(message="Delivery Complete"), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3003)
